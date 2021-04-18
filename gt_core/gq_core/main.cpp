fcaf78e86cc2a25515aa01caa8c9f93e864b597b
Ahonen Mika J <mika.ahonen@helsinki.fi>
2021-04-19 02:21:12 +0300

    Added forgotten main

---
+++ gt_core/gq_core/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "Starting to run core player...";
+
+    return a.exec();
+}
